a57860afceb121886f476d0eb7ef502f3cf06bc6
khaliliali <ali.khalili@outlook.com>
2023-06-02 22:48:06 +0800

    Upload a blob

--- Azure_BlobApp/Program.cs
+++ Azure_BlobApp/Program.cs
@@ -3,13 +3,27 @@ using Azure.Storage.Blobs.Models;
 
 string connectionString = "";
 string containerName = "";
+string blobName = "";
+string filePath = "";
 
 /* Create Container */
+
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 //await blobServiceClient.CreateBlobContainerAsync(containerName);
 //Console.WriteLine("Container Created... ");
 
+
 /* Create Container and Manageing the access */
+
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 //await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
 //Console.WriteLine("Container Created and managing access ... ");
+
+
+/* Uploading a Blob */
+
+BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+await blobClient.UploadAsync(filePath);
+Console.WriteLine("Uploaded the Blob");
+
