9ff9f228d9cbccbaff59cb0047bf7eaf70933120
khaliliali <ali.khalili@outlook.com>
2023-06-02 22:41:51 +0800

    Create Container and managing access

--- Azure_BlobApp/Program.cs
+++ Azure_BlobApp/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+string connectionString = "";
+string containerName = "";
+
+/* Create Container */
+//BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+//await blobServiceClient.CreateBlobContainerAsync(containerName);
+//Console.WriteLine("Container Created... ");
+
+/* Create Container and Manageing the access */
+//BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+//await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
+//Console.WriteLine("Container Created and managing access ... ");
